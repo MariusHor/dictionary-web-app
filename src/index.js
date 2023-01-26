deca1374f899c989f2ebbfa1969f4b5c6c766369
mariushor <marius.horghidan@yahoo.com>
2023-01-26 13:00:36 +0200

    Created the initial html structure with the header and search form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './styles/main.scss';
 import './js/app';
+import './assets/favicon-32x32.png';
+import './assets/icon-arrow-down.svg';
+import './assets/logo.svg';
+import './assets/icon-search.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
